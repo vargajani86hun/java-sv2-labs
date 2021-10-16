ce30322b8f3857b406544954cdc942366bc934d6
JaniVarga86HUN <jani.varga86@gmail.com>
2021-10-16 17:58:44 +0200

    The classstructureintegrate.BankAccount class is created.

---
+++ weekly_projects01/src/main/java/classstructureintegrate/BankAccount.java
@@ -0,0 +1,25 @@
+package classstructureintegrate;
+
+public class BankAccount {
+
+    private String accountNumber, owner;
+    private int balance;
+
+    public BankAccount(String accountNumber, String owner, int balance) {
+        this.accountNumber = accountNumber;
+        this.owner = owner;
+        this.balance = balance;
+    }
+
+    public void deposit(int amount){
+
+    }
+
+    public void withdraw(int amount) {
+
+    }
+
+    public String getInfo(){
+        return "";
+    }
+}
