8ca518046a2c095d1d37b663cfa13ab4ad467e7e
JaniVarga86HUN <jani.varga86@gmail.com>
2021-10-16 20:36:52 +0200

    The LocalVariablesMain class is created.

---
+++ weekly_projects01/src/main/java/localvariables/LocalVariablesMain.java
@@ -0,0 +1,10 @@
+package localvariables;
+
+public class LocalVariablesMain {
+
+    public static void main(String[] args) {
+        boolean b ;
+        System.out.println("A b értéke: " + b);
+
+    }
+}
