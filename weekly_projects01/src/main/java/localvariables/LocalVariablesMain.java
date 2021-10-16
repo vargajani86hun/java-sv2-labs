946327ee9c0d415ebb14a53c6300f050b8e082f3
JaniVarga86HUN <jani.varga86@gmail.com>
2021-10-16 20:55:36 +0200

    The printing command to variable 'x' is added after the block.

--- weekly_projects01/src/main/java/localvariables/LocalVariablesMain.java
+++ weekly_projects01/src/main/java/localvariables/LocalVariablesMain.java
@@ -23,5 +23,6 @@ public class LocalVariablesMain {
             System.out.println("Az x értéke: " + x);
             System.out.println("Az a változó érétke: " + a);
         }
+        System.out.println("Az x értéke: " + x);
     }
 }
