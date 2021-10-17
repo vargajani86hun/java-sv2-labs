84fd68e03316e48656b8e4528496763e4a5ceb41
JaniVarga86HUN <jani.varga86@gmail.com>
2021-10-17 16:17:51 +0200

    The array.ArrayHandler class is created, the 2nd task is ready.

--- weekly_projects01/src/main/java/array/ArrayHandler.java
+++ weekly_projects01/src/main/java/array/ArrayHandler.java
@@ -1,9 +1,18 @@
 package array;
 
+import java.lang.reflect.Array;
+
 public class ArrayHandler {
     void addIndexToNumber(int[] source) {
         for (int i = 0; i < source.length; i++) {
             source[i] += i;
         }
     }
+
+    void concatenateIndexToWord(String[] source) {
+        for (int i = 0; i < source.length; i++) {
+            source[i] = i + source[i];
+        }
+    }
+
 }
