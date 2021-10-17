973fdab5767e01b94604f76c583eb0cf84d251e2
JaniVarga86HUN <jani.varga86@gmail.com>
2021-10-17 02:24:54 +0200

    Constructors and getters are added.

--- weekly_projects01/src/main/java/packages/lastname/LastName.java
+++ weekly_projects01/src/main/java/packages/lastname/LastName.java
@@ -3,4 +3,11 @@ package packages.lastname;
 public class LastName {
     private String last;
 
+    public LastName(String last) {
+        this.last = last;
+    }
+
+    public String getLast() {
+        return last;
+    }
 }
