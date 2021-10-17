011b6a7117b1c5e2ef5569c8934548413a5961ea
JaniVarga86HUN <jani.varga86@gmail.com>
2021-10-17 16:18:18 +0200

    The array.ArrayHandler class is created, the 3rd task is ready.

--- weekly_projects01/src/main/java/array/ArrayHandler.java
+++ weekly_projects01/src/main/java/array/ArrayHandler.java
@@ -15,4 +15,37 @@ public class ArrayHandler {
         }
     }
 
+    public static void main(String[] args) {
+        int[] numbers = {1, 1, 2, 3, 5, 8, 13};
+        String[] words = {"egy", "kető", "három", "négy", "öt"};
+        ArrayHandler arrayHandler = new ArrayHandler();
+
+        arrayHandler.addIndexToNumber(numbers);
+        arrayHandler.concatenateIndexToWord(words);
+
+        for (int number: numbers) {
+            System.out.println(number);
+        }
+        for (String word: words) {
+            System.out.println(word);
+        }
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (i < numbers.length - 1) {
+                System.out.print(numbers[i] + ", ");
+            }
+            else {
+                System.out.print(numbers[i]);
+            }
+        }
+        System.out.println();
+        for (int i = 0; i < words.length; i++) {
+            if (i < words.length - 1){
+                System.out.print(words[i] + ", ");
+            }
+            else {
+                System.out.print(words[i]);
+            }
+        }
+    }
 }
