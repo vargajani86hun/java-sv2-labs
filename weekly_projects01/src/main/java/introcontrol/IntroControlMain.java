59a4a749dfdef2cf19d43006235c27e7f12d4b6f
JaniVarga86HUN <jani.varga86@gmail.com>
2021-10-17 14:12:02 +0200

    In introcontrol.IntroControlMain class, the test of 2nd method is ready.

--- weekly_projects01/src/main/java/introcontrol/IntroControlMain.java
+++ weekly_projects01/src/main/java/introcontrol/IntroControlMain.java
@@ -4,5 +4,7 @@ public class IntroControlMain {
     public static void main(String[] args) {
         IntroControl ic = new IntroControl();
         System.out.println(ic.subtractTenIfGreaterThanTen(25));
+
+        System.out.println(ic.describeNumber(1));
     }
 }
