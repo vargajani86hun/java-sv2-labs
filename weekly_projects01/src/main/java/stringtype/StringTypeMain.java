11bc9786f27eda974e366f7b9254ddda9ee66782
JaniVarga86HUN <jani.varga86@gmail.com>
2021-10-17 10:28:44 +0200

    Class StringTypeMain is ready.

--- weekly_projects01/src/main/java/stringtype/StringTypeMain.java
+++ weekly_projects01/src/main/java/stringtype/StringTypeMain.java
@@ -9,11 +9,16 @@ public class StringTypeMain {
         message = message + 444;
         boolean b = ("Hello John Doe").equals(message);
         boolean c = ("Hello John Doe444").equals(message);
+
         System.out.println(prefix + name);
         System.out.println(message);
         System.out.println(b);
         System.out.println(c);
-        message = "" + "";
-        System.out.println(message + ", " + message.length());
+
+        System.out.println("" + "" + ", " + ("" + "").length());
+
+        System.out.println("Abcde".length());
+        System.out.println("Abcde".substring(0,1) +"Abcde".substring(2,3));
+        System.out.println("Abcde".substring(0,3));
     }
 }
