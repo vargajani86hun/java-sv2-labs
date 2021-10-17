7eff212cde5b809ef1d833aebaff0cfa2e0ec688
JaniVarga86HUN <jani.varga86@gmail.com>
2021-10-17 15:44:30 +0200

    The array.ArrayMain class is created. The 1st task is ready.

---
+++ weekly_projects01/src/main/java/array/ArrayMain.java
@@ -0,0 +1,8 @@
+package array;
+
+public class ArrayMain {
+    public static void main(String[] args) {
+        String[] daysOfWeek = {"hétfő", "kedd", "szerda", "csütörtök", "péntek", "szombat", "vasárnap"};
+        System.out.println(daysOfWeek[1] + ", " + daysOfWeek.length);
+    }
+}
