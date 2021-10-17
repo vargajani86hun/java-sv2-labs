f51e884975bad86185f9368f821d839aa4091d20
JaniVarga86HUN <jani.varga86@gmail.com>
2021-10-17 15:54:57 +0200

    In the array.ArrayMain class, the 2nd task is ready.

--- weekly_projects01/src/main/java/array/ArrayMain.java
+++ weekly_projects01/src/main/java/array/ArrayMain.java
@@ -4,5 +4,19 @@ public class ArrayMain {
     public static void main(String[] args) {
         String[] daysOfWeek = {"hétfő", "kedd", "szerda", "csütörtök", "péntek", "szombat", "vasárnap"};
         System.out.println(daysOfWeek[1] + ", " + daysOfWeek.length);
+
+        int[] powersOfTwo = new int[5];
+        for (int i = 0; i < 5; i++) {
+            if (i == 0) {
+                powersOfTwo[i] = 1;
+            }
+            else {
+                powersOfTwo[i] = powersOfTwo[i-1] * 2;
+            }
+        }
+
+        for (int powers: powersOfTwo) {
+            System.out.print(powers + " ");
+        }
     }
 }
