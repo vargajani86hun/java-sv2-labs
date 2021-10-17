8825065583e062db169a157c34b0acd67337c16a
JaniVarga86HUN <jani.varga86@gmail.com>
2021-10-17 03:09:54 +0200

    The class javaapi.Upper is ready.

---
+++ weekly_projects01/src/main/java/javaapi/Upper.java
@@ -0,0 +1,12 @@
+package javaapi;
+
+import java.util.Locale;
+
+public class Upper {
+
+    public static void main(String[] args) {
+        String s = "Hello World!";
+        s = s.toUpperCase();
+        System.out.println(s);
+    }
+}
