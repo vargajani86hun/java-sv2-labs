f6e2a463b63fc3890ef42500b1e1b351809e1690
JaniVarga86HUN <jani.varga86@gmail.com>
2021-10-17 17:30:43 +0200

    The class arrayofarrays.DailyValues is ready. In the class arrayofarrays.Rectangular, typos fixed.

--- weekly_projects01/src/main/java/arrayofarrays/Rectangle.java
+++ weekly_projects01/src/main/java/arrayofarrays/Rectangle.java
@@ -2,14 +2,14 @@ package arrayofarrays;
 
 public class Rectangle {
     static int[][] rectangularMatrix(int size) {
-        int[][] tMatrix = new int[size][];
+        int[][] rMatrix = new int[size][];
         for (int i = 0; i < size; i++) {
-            tMatrix[i] = new int[size];
+            rMatrix[i] = new int[size];
             for (int j = 0; j < size; j++){
-                tMatrix[i][j] = i;
+                rMatrix[i][j] = i;
             }
         }
-        return tMatrix;
+        return rMatrix;
     }
 
     public static void main(String[] args) {
