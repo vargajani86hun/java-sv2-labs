b82e9ae57d21dac8433fd35f0e473e3471451c0e
JaniVarga86HUN <jani.varga86@gmail.com>
2021-10-17 10:08:09 +0200

    Message variable is added.

--- weekly_projects01/src/main/java/stringtype/StringTypeMain.java
+++ weekly_projects01/src/main/java/stringtype/StringTypeMain.java
@@ -5,5 +5,6 @@ public class StringTypeMain {
     public static void main(String[] args) {
         String prefix = "Hello ";
         String name = "John Doe";
+        String message = prefix + name;
     }
 }
