230ada991da30d5b13a3d0a1e2fa7176b513705d
JaniVarga86HUN <jani.varga86@gmail.com>
2021-10-17 14:02:08 +0200

    The introcontrol.IntroControlMain class is created. The test of first method is ready.

---
+++ weekly_projects01/src/main/java/introcontrol/IntroControlMain.java
@@ -0,0 +1,8 @@
+package introcontrol;
+
+public class IntroControlMain {
+    public static void main(String[] args) {
+        IntroControl ic = new IntroControl();
+        System.out.println(ic.subtractTenIfGreaterThanTen(25));
+    }
+}
