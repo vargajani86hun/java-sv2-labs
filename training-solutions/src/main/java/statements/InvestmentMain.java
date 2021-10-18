a0cac9db68e6a00119225b8a4853ef0c49bd10cb
JaniVarga86HUN <jani.varga86@gmail.com>
2021-10-18 21:04:51 +0200

    In class statements.InvestmentMain, prints are modified to the waited form.

--- training-solutions/src/main/java/statements/InvestmentMain.java
+++ training-solutions/src/main/java/statements/InvestmentMain.java
@@ -16,21 +16,30 @@ public class InvestmentMain {
         interestRate = scr.nextInt();
         scr.nextLine();
         investment = new Investment(fund, interestRate);
-
+        System.out.println("Befektetés összege:\n" + fund + "\nKamatláb:\n" + interestRate);
+/*
         System.out.println("Adja meg, hány nap hozamára kíváncsi: ");
         days = scr.nextInt();
         scr.nextLine();
+        */
+        days = 50;
         System.out.println("Tőke: " + investment.getFund());
         System.out.println("Hozam " + days + " napra: " + investment.getYield(days));
-
+/*
         System.out.println("Adja meg, hány nap után szeretné kivenni befektetését: ");
         days = scr.nextInt();
         scr.nextLine();
+
+ */
+        days = 80;
         System.out.println("Kivett összeg " + days + " nap után: " + investment.close(days));
-
+/*
         System.out.println("Adja meg, hány nap után szeretné kivenni befektetését: ");
         days = scr.nextInt();
         scr.nextLine();
+
+ */
+        days = 90;
         System.out.println("Kivett összeg " + days + " nap után: " + investment.close(days));
 
     }
