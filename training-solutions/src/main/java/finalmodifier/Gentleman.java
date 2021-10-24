73d6f65f9d705cae1db18a5bd3984410f5903ee6
vargajani86hun <jani.varga86@gmail.com>
2021-10-24 19:11:45 +0200

    Selfcheck is updated. finalmodifier.Gentleman is read.

---
+++ training-solutions/src/main/java/finalmodifier/Gentleman.java
@@ -0,0 +1,13 @@
+package finalmodifier;
+
+public class Gentleman {
+    public static final String MESSAGE_PREFIX = "Üdvözlöm ";
+    public String sayHello(String name) {
+        return MESSAGE_PREFIX + name;
+    }
+
+    public static void main(String[] args) {
+        Gentleman gentleman = new Gentleman();
+        System.out.println(gentleman.sayHello("Johne Watson"));
+    }
+}
