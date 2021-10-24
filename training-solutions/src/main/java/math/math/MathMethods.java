1b04ce141a33f7782c3fb91036750ee460c0b79a
vargajani86hun <jani.varga86@gmail.com>
2021-10-24 21:00:15 +0200

    Class math.math.MathMethods is ready.

---
+++ training-solutions/src/main/java/math/math/MathMethods.java
@@ -0,0 +1,20 @@
+package math.math;
+
+public class MathMethods {
+    public static void main(String[] args) {
+        System.out.println(Math.max(13, 7));
+        System.out.println(Math.min(13, 7));
+        System.out.println(Math.PI);
+        System.out.println(Math.round(Math.PI));
+        System.out.println(Math.round(2.743));
+        System.out.println(Math.abs(-13.5));
+        System.out.println(Math.negateExact(33));
+        System.out.println(Math.addExact(21, 12));
+        System.out.println(Math.subtractExact(21, 12));
+        System.out.println(Math.multiplyExact(21, 12));
+        System.out.println(Math.pow(2.5, 2));
+        System.out.println(Math.incrementExact(21));
+        System.out.println(Math.decrementExact(21));
+        System.out.println(Math.random());
+    }
+}
