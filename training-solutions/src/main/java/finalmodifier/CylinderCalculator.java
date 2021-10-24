36132ad156fd9fe78a2c4c25ed3a718fa4ada47e
vargajani86hun <jani.varga86@gmail.com>
2021-10-24 19:20:56 +0200

    Classes finalmodifier.CircleCalculato and CylinderCalculator are read.

---
+++ training-solutions/src/main/java/finalmodifier/CylinderCalculator.java
@@ -0,0 +1,11 @@
+package finalmodifier;
+
+public class CylinderCalculator {
+    public double calculateVolume(double r, double h) {
+        return r * r * h * CircleCalculator.PI;
+    }
+
+    public double calculateSurfaceArea(double r, double h) {
+        return 2 * r * h * CircleCalculator.PI;
+    }
+}
