2054a5f4c891ebdbafaace277d838d9fd3656822
JaniVarga86HUN <jani.varga86@gmail.com>
2021-10-29 19:58:57 +0200

    The 1st task of intro methodes is ready.

---
+++ training-solutions/src/main/java/intromethods/employee/EmployeeMain.java
@@ -0,0 +1,9 @@
+package intromethods.employee;
+
+public class EmployeeMain {
+    public static void main(String[] args) {
+        Employee employee = new Employee("Johne Doe", 1993, 12000);
+        System.out.println(employee);
+
+    }
+}
