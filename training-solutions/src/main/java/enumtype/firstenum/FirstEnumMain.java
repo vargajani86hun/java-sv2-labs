64a227ef8435f02e66a6e729542ba42894747d22
vargajani86hun <jani.varga86@gmail.com>
2021-11-01 13:43:59 +0100

    The 2nd task of lesson enum types is ready.

--- training-solutions/src/main/java/enumtype/firstenum/FirstEnumMain.java
+++ training-solutions/src/main/java/enumtype/firstenum/FirstEnumMain.java
@@ -9,5 +9,11 @@ public class FirstEnumMain {
             System.out.println(c);
         }
         System.out.println(Arrays.toString(continents));
+
+        System.out.println(Ocean.valueOf("ARCTIC"));
+        System.out.println(Ocean.valueOf("ATLANTIC"));
+        System.out.println(Ocean.valueOf("INDIAN"));
+        System.out.println(Ocean.valueOf("PACIFIC"));
+        System.out.println(Ocean.valueOf("SOUTHERN"));
     }
 }
