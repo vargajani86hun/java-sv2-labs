099236350336f7439d8a5acd5bf5311bef8c56e4
vargajani86hun <jani.varga86@gmail.com>
2021-11-01 04:09:20 +0100

    The 2nd task of simple types is ready.

--- training-solutions/src/main/java/primitivetypes/PrimitiveTypesMain.java
+++ training-solutions/src/main/java/primitivetypes/PrimitiveTypesMain.java
@@ -3,24 +3,42 @@ package primitivetypes;
 public class PrimitiveTypesMain {
     public static void main(String[] args) {
         int a = 2;
-        System.out.println(a);
+        System.out.println("a: " + a);
         Integer b = 2;
-        System.out.println(b);
+        System.out.println("b: " + b);
         Integer c = a;
-        System.out.println(c);
+        System.out.println("c: " + c);
         Integer d = b.intValue();
-        System.out.println(d);
+        System.out.println("d: " + d);
         Integer e = Integer.valueOf(2);
-        System.out.println(e);
+        System.out.println("e: " + e);
         int f = Integer.valueOf(2);
-        System.out.println(f);
+        System.out.println("f: " + f);
         int g = Integer.valueOf(a);
-        System.out.println(g);
+        System.out.println("g: " + g);
         int h = Integer.valueOf(c);
-        System.out.println(h);
+        System.out.println("h: " + h);
         int i = Integer.parseInt("2");
-        System.out.println(i);
+        System.out.println("i: " + i);
         Integer j = Integer.parseInt("2");
-        System.out.println(j);
+        System.out.println("j: " + j);
+        //End of the 1st task
+
+        boolean k = true;
+        System.out.println("k: " + k);
+        Boolean l = true;
+        System.out.println("l: " + l);
+        boolean m = Boolean.valueOf(true);
+        System.out.println("m: " + m);
+        Boolean n = Boolean.valueOf(true);
+        System.out.println("n: " + n);
+        boolean o = Boolean.parseBoolean("TrUe");
+        System.out.println("o: " + o);
+        Boolean p = Boolean.parseBoolean("TrUe");
+        System.out.println("p: " + p);
+        boolean q = Boolean.parseBoolean("TreU");
+        System.out.println("q: " + q);
+        Boolean r = Boolean.parseBoolean("TreU");
+        System.out.println("r: " + r);
     }
 }
