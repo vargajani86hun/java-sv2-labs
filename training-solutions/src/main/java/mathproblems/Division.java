6c390e14d4a237a27ca03b58ff86fb8ba8baec25
vargajani86hun <jani.varga86@gmail.com>
2021-11-02 00:49:43 +0100

    The 2nd task of lesson divisibility test is ready.

--- training-solutions/src/main/java/mathproblems/Division.java
+++ training-solutions/src/main/java/mathproblems/Division.java
@@ -9,10 +9,22 @@ public class Division {
         }
     }
 
+    public void getRightNumbers(int[] numbers) {
+        for (int i = 1; i < numbers.length; i++) {
+            if (numbers[i] % i == 0) {
+                System.out.print(numbers[i] + " ");
+            }
+        }
+    }
+
     public static void main(String[] args) {
         Division division = new Division();
         division.getDivisors(12);
         division.getDivisors(15);
         division.getDivisors(19);
+
+        int[] numbers = {2, 5, 23, 56, -36, 45, 78, 14, -8, 4, 10};
+        division.getRightNumbers(numbers);
+        System.out.println();
     }
 }
