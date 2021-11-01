19ec48d8352ea2d2e8ab7da224050f2dfd031507
vargajani86hun <jani.varga86@gmail.com>
2021-11-01 21:41:07 +0100

    The value attribute of SolarSystem is renamed to numberOfMoons. Refactoring is referred to that.

--- training-solutions/src/main/java/enumtype/solarsystem/SolarSystemMain.java
+++ training-solutions/src/main/java/enumtype/solarsystem/SolarSystemMain.java
@@ -4,7 +4,8 @@ public class SolarSystemMain {
     public static void main(String[] args) {
         SolarSystem[] planets = SolarSystem.values();
         for (SolarSystem planet : planets) {
-            System.out.println(planet.name() + " holdjainak száma: " + planet.getValue());
+            System.out.println(planet.name() + " has "
+                    + planet.getNumberOfMoons() + " moons.");
         }
     }
 }
