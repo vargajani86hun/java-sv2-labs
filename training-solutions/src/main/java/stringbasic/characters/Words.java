44ede1efcacf6e92692f589f82b485c6b2d86d70
vargajani86hun <jani.varga86@gmail.com>
2021-11-02 09:51:05 +0100

    The 2n task of lesson string basics is ready.

--- training-solutions/src/main/java/stringbasic/characters/Words.java
+++ training-solutions/src/main/java/stringbasic/characters/Words.java
@@ -8,5 +8,19 @@ public class Words {
         System.out.println("Adja meg a feladatnak szánt szót:");
         String task = scr.nextLine();
 
+        char[] letters = new char[task.length()];
+        System.out.println("Gépelje be a betűnként '" + task + "' szót!");
+        for (int i = 0; i < letters.length; i++) {
+            letters[i] = scr.nextLine().charAt(0);
+        }
+
+        String solution = new String(letters);
+        if (task.equals(solution)){
+            System.out.println("Helyesen betűzted!");
+        }
+        else {
+            System.out.println("Ezt most elrontottad. Ezt írtad be: " +
+                    solution + ", a feladat viszont '" + task + "' volt.");
+        }
     }
 }
