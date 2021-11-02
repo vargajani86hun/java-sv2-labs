b4cfe0151b3fe618169d525d150203ff4cb5131e
vargajani86hun <jani.varga86@gmail.com>
2021-11-02 03:26:47 +0100

    The 2n task of lesson string basics is started.

---
+++ training-solutions/src/main/java/stringbasic/characters/Words.java
@@ -0,0 +1,12 @@
+package stringbasic.characters;
+
+import java.util.Scanner;
+
+public class Words {
+    public static void main(String[] args) {
+        Scanner scr = new Scanner(System.in);
+        System.out.println("Adja meg a feladatnak szánt szót:");
+        String task = scr.nextLine();
+
+    }
+}
