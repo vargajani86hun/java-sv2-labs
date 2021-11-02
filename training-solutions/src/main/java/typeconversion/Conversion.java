3f60995333eace0b65dab4821b1d2554081ff0b7
vargajani86hun <jani.varga86@gmail.com>
2021-11-02 01:29:58 +0100

    The 1st task of lesson type conversion is ready.

---
+++ training-solutions/src/main/java/typeconversion/Conversion.java
@@ -0,0 +1,8 @@
+package typeconversion;
+
+public class Conversion {
+    public double convertDoubleToDouble(double number) {
+        int intNumberPart = (int) number;
+        return (double) intNumberPart;
+    }
+}
---
+++ training-solutions/src/main/java/typeconversion/ConversionMain.java
@@ -0,0 +1,8 @@
+package typeconversion;
+
+public class ConversionMain {
+    public static void main(String[] args) {
+        Conversion conv = new Conversion();
+        System.out.println(conv.convertDoubleToDouble(3.111));
+    }
+}
