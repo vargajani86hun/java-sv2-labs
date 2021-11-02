c164437942f469c6e6c07d7f6c64206d798d6f1b
vargajani86hun <jani.varga86@gmail.com>
2021-11-02 01:37:50 +0100

    The 2nd task of lesson type conversion is ready.

--- training-solutions/src/main/java/typeconversion/Conversion.java
+++ training-solutions/src/main/java/typeconversion/Conversion.java
@@ -5,4 +5,17 @@ public class Conversion {
         int intNumberPart = (int) number;
         return (double) intNumberPart;
     }
+
+    public byte[] convertIntArrayToByteArray(int[] numbers) {
+        byte[] byteNumbers = new byte[numbers.length];
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] >= 0 && numbers[i] < 128) {
+                byteNumbers[i] = (byte) numbers[i];
+            }
+            else {
+                byteNumbers[i] = -1;
+            }
+        }
+        return byteNumbers;
+    }
 }
--- training-solutions/src/main/java/typeconversion/ConversionMain.java
+++ training-solutions/src/main/java/typeconversion/ConversionMain.java
@@ -1,8 +1,12 @@
 package typeconversion;
 
+import java.util.Arrays;
+
 public class ConversionMain {
     public static void main(String[] args) {
         Conversion conv = new Conversion();
         System.out.println(conv.convertDoubleToDouble(3.111));
+        int[] numbers = {5, -14, 6, 2, 125, 354, 9738, 3};
+        System.out.println(Arrays.toString(conv.convertIntArrayToByteArray(numbers)));
     }
 }
