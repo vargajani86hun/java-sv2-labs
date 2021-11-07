a6e0539d3a1114a63d3a892fa61fb16e35d23706
JaniVarga86HUN <jani.varga86@gmail.com>
2021-11-07 02:21:08 +0100

    The test of greetings is ready.

--- training-solutions/controlselection/src/test/java/controlselection/greetings/GreetingsTest.java
+++ training-solutions/controlselection/src/test/java/controlselection/greetings/GreetingsTest.java
@@ -15,41 +15,19 @@ public class GreetingsTest {
     }
 
     @Test
-    void testMorning() {
+    void testGreetings() {
         assertEquals("jó reggelt", greetings.sayGreetings(5, 0));
         assertEquals("jó reggelt", greetings.sayGreetings(8, 50));
-        assertNotEquals("jó reggelt", greetings.sayGreetings(9, 0));
-        assertNotEquals("jó reggelt", greetings.sayGreetings(4, 30));
-        assertNotEquals("jó reggelt", greetings.sayGreetings(18, 35));
-    }
 
-    @Test
-    void testDay() {
         assertEquals("jó napot", greetings.sayGreetings(9, 0));
         assertEquals("jó napot", greetings.sayGreetings(14, 10));
         assertEquals("jó napot", greetings.sayGreetings(18, 30));
-        assertNotEquals("jó napot", greetings.sayGreetings(18,31));
-        assertNotEquals("jó napot", greetings.sayGreetings(20,31));
-        assertNotEquals("jó napot", greetings.sayGreetings(5,31));
-        assertNotEquals("jó napot", greetings.sayGreetings(3,31));
-    }
 
-    @Test
-    void testEvening() {
         assertEquals("jó estét", greetings.sayGreetings(18, 31));
         assertEquals("jó estét", greetings.sayGreetings(19, 31));
-        assertNotEquals("jó estét", greetings.sayGreetings(20, 0));
-        assertNotEquals("jó estét", greetings.sayGreetings(18, 0));
-        assertNotEquals("jó estét", greetings.sayGreetings(8, 0));
-        assertNotEquals("jó estét", greetings.sayGreetings(2, 0));
-    }
 
-    @Test
-    void testNight() {
         assertEquals("jó éjt", greetings.sayGreetings(20, 0));
         assertEquals("jó éjt", greetings.sayGreetings(4, 0));
-        assertNotEquals("jó éjt", greetings.sayGreetings(5, 10));
-        assertNotEquals("jó éjt", greetings.sayGreetings(12, 10));
-        assertNotEquals("jó éjt", greetings.sayGreetings(19, 10));
+
     }
 }
