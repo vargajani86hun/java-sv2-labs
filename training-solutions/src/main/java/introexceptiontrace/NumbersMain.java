55bd2616b026a59889b0b923ee266aa57912d77d
JaniVarga86HUN <jani.varga86@gmail.com>
2021-11-18 18:34:34 +0100

    The 2nd task of exception in stacktrace is ready.

---
+++ training-solutions/src/main/java/introexceptiontrace/NumbersMain.java
@@ -0,0 +1,8 @@
+package introexceptiontrace;
+
+public class NumbersMain {
+    public static void main(String[] args) {
+        Change change = new Change();
+        System.out.println(change.changeNumbers());
+    }
+}
