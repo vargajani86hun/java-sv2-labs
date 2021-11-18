702d3a273285771554cdf090e0859248200ec2a1
JaniVarga86HUN <jani.varga86@gmail.com>
2021-11-18 18:41:27 +0100

    Printing of the array is repaired.

--- training-solutions/src/main/java/introexceptiontrace/NumbersMain.java
+++ training-solutions/src/main/java/introexceptiontrace/NumbersMain.java
@@ -1,8 +1,11 @@
 package introexceptiontrace;
 
+import java.util.Arrays;
+
 public class NumbersMain {
     public static void main(String[] args) {
         Change change = new Change();
-        System.out.println(change.changeNumbers());
+        int[] numbers = change.changeNumbers();
+        System.out.println(Arrays.toString(numbers));
     }
 }
