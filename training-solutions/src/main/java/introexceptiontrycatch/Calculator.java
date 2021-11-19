878e708d2ba0140f494c96f5df8bb63ff55839c3
JaniVarga86HUN <jani.varga86@gmail.com>
2021-11-19 07:33:40 +0100

    Calculator fixed - catching InputMismatchException is added.

--- training-solutions/src/main/java/introexceptiontrycatch/Calculator.java
+++ training-solutions/src/main/java/introexceptiontrycatch/Calculator.java
@@ -1,29 +1,32 @@
 package introexceptiontrycatch;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[] args) {
         Scanner scr = new Scanner(System.in);
-        System.out.println("Adjon meg két egész számot: ");
-        int a = scr.nextInt();
-        int b = scr.nextInt();
-        scr.nextLine();
-        System.out.println("Adja meg az elvégzendő műveletet ( + | - | * | / )");
+        Calculator calc = new Calculator();
         try {
+            int[] numbers = calc.readNumbers();
+            System.out.println("Adja meg az elvégzendő műveletet ( + | - | * | / )");
             char operator = scr.nextLine().charAt(0);
             switch (operator) {
                 case '+' :
-                    System.out.println(a + " + " + b + " = " + (a + b));
+                    System.out.println(numbers[0] + " + " + numbers[1] +
+                            " = " + (numbers[0] + numbers[1]));
                     break;
                 case '-' :
-                    System.out.println(a + " - " + b + " = " + (a - b));
+                    System.out.println(numbers[0] + " - " + numbers[1] +
+                            " = " + (numbers[0] - numbers[1]));
                     break;
                 case '*' :
-                    System.out.println(a + " * " + b + " = " + (a * b));
+                    System.out.println(numbers[0] + " * " + numbers[1] +
+                            " = " + (numbers[0] * numbers[1]));
                     break;
                 case '/' :
-                    System.out.println(a + " / " + b + " = " + (a / b));
+                    System.out.println(numbers[0] + " / " + numbers[1] +
+                            " = " + (numbers[0] / numbers[1]));
                     break;
                 default :
                     System.out.println("Ismeretlen művelet");
@@ -38,5 +41,19 @@ public class Calculator {
             System.out.println("Nullával való osztás nem lehetséges!");
             System.out.println(ae.getMessage());
         }
+        catch (InputMismatchException ime) {
+            System.out.println("Nem egész számo(ka)t adott meg!");
+            System.out.println(ime.getMessage());
+        }
+    }
+
+    private int[] readNumbers() {
+        int[] results = new int[2];
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Adjon meg két számot:");
+        for (int i = 0; i < 2; i++) {
+            results[i] = sc.nextInt();
+        }
+        return results;
     }
 }
