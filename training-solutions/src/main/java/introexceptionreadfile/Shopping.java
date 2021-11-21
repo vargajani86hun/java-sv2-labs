d3bf756467301f902d90d0dddf3061da8b90195b
vargajani86hun <jani.varga86@gmail.com>
2021-11-21 14:13:33 +0100

    The 2nd task of read file is ready.

---
+++ training-solutions/src/main/java/introexceptionreadfile/Shopping.java
@@ -0,0 +1,21 @@
+package introexceptionreadfile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Shopping {
+    public static void main(String[] args) {
+        try {
+            List<String> toBuy = Files.readAllLines(Paths.get(
+                    "src/main/texts/shoppinglist.txt"));
+            for (String item : toBuy) {
+                System.out.println(item);
+            }
+        }
+        catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }
+}
