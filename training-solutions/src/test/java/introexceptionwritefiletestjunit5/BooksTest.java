fa6ec8b64682e0993c027859b30f83294598ddd7
JaniVarga86HUN <jani.varga86@gmail.com>
2021-11-23 11:35:25 +0100

    The 2nd task of test writing file with JUnit5 is ready.

--- training-solutions/src/test/java/introexceptionwritefiletestjunit5/BooksTest.java
+++ training-solutions/src/test/java/introexceptionwritefiletestjunit5/BooksTest.java
@@ -1,4 +1,32 @@
 package introexceptionwritefiletestjunit5;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class BooksTest {
+
+    @TempDir
+    File tempFolder;
+
+    @Test
+    void testWriteList() throws IOException {
+        Path path = tempFolder.toPath().resolve("tempBooks.txt");
+        Books books = new Books();
+        books.createBookList(Paths.get("src/test/texts/books.txt"));
+        books.getReadableBookList();
+
+        books.writeList(path);
+        List<String> lines = Files.readAllLines(path);
+
+        assertEquals(books.getReadableBookList(),lines);
+    }
 }
