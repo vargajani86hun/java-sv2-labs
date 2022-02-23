e13d91786127c57c9378eb008f199ae61393a3fe
JaniVarga86HUN <jani.varga86@gmail.com>
2022-02-23 18:52:28 +0100

    Typo is fixed.

--- training-solutions/src/test/resources/db/migration/V1__activities.sql
+++ training-solutions/src/test/resources/db/migration/V1__activities.sql
@@ -1,3 +1,3 @@
-CREATE TABLE activities(id BIGINT AUTO_INCREMENT, start_time DATESTAMP,
+CREATE TABLE activities(id BIGINT AUTO_INCREMENT, start_time TIMESTAMP,
     description VARCHAR(255), activity_type VARCHAR(20),
     CONSTRAINT pk_activities PRIMARY KEY(id));
