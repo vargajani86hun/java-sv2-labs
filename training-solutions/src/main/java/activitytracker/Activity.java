e4ae1a107ea24c7eb2d8b208282fe7234669473d
JaniVarga86HUN <jani.varga86@gmail.com>
2022-02-23 18:47:46 +0100

    The equals and hashCode methods are added.

--- training-solutions/src/main/java/activitytracker/Activity.java
+++ training-solutions/src/main/java/activitytracker/Activity.java
@@ -3,6 +3,7 @@ package activitytracker;
 import activity.ActivityType;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 public class Activity {
     private long id;
@@ -41,5 +42,17 @@ public class Activity {
                 ", description='" + description + '\'' +
                 ", type=" + type +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Activity activity)) return false;
+        return id == activity.id && Objects.equals(startTime, activity.startTime) && Objects.equals(description, activity.description) && type == activity.type;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, startTime, description, type);
     }
 }
