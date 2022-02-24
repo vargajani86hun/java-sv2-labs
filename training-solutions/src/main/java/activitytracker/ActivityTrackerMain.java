872508a1a8e68ccbd18b7bea8512c23e1b3f55d5
JaniVarga86HUN <jani.varga86@gmail.com>
2022-02-24 19:57:14 +0100

    The 2nd task of lesson 'Simple JDBC architect' is ready.

--- training-solutions/src/main/java/activitytracker/ActivityTrackerMain.java
+++ training-solutions/src/main/java/activitytracker/ActivityTrackerMain.java
@@ -4,6 +4,7 @@ import activity.ActivityType;
 import org.mariadb.jdbc.MariaDbDataSource;
 
 import javax.sql.DataSource;
+import javax.xml.crypto.Data;
 import java.sql.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -11,13 +12,13 @@ import java.util.List;
 
 public class ActivityTrackerMain {
     public static void main(String[] args) {
-        Activity biking1 = new Activity(1, LocalDateTime.of(2021, 10, 22, 10, 30),
+        Activity biking1 = new Activity(LocalDateTime.of(2021, 10, 22, 10, 30),
                 "Pallag oda-vissza", ActivityType.BIKING);
-        Activity running = new Activity(2, LocalDateTime.of(2021, 10, 28, 16,0),
+        Activity running = new Activity(LocalDateTime.of(2021, 10, 28, 16,0),
                 "Belvárosi kocogás", ActivityType.RUNNING);
-        Activity biking2 = new Activity(3, LocalDateTime.of(2021, 11, 3, 11, 10),
+        Activity biking2 = new Activity(LocalDateTime.of(2021, 11, 3, 11, 10),
                 "Józsa oda-vissza", ActivityType.BIKING);
-        Activity basketBall = new Activity(4, LocalDateTime.of(2021, 12, 3, 16, 45),
+        Activity basketBall = new Activity(LocalDateTime.of(2021, 12, 3, 16, 45),
                 "Csapat építés", ActivityType.BASKETBALL);
         ActivityTrackerMain atm = new ActivityTrackerMain();
 
@@ -27,7 +28,10 @@ public class ActivityTrackerMain {
         atm.insertActivityToDB(biking2, ds);
         atm.insertActivityToDB(basketBall, ds);
 
-        System.out.println(atm.getActivitiesFromDB(ds));
+        List<Activity> activities = atm.getActivitiesFromDB(ds);
+        activities.stream().map(Activity::getId).map(id -> atm.getActivityById(id, ds))
+                .forEach(System.out::println);
+
 
     }
 
@@ -65,6 +69,31 @@ public class ActivityTrackerMain {
              ResultSet rs = stmt.executeQuery("SELECT * FROM activities")) {
 
             return getActivitiesFromResultSet(rs);
+
+        } catch (SQLException sqle) {
+            throw new IllegalStateException("Unable to read DB", sqle);
+        }
+    }
+
+    public Activity getActivityById(long id, DataSource ds) {
+        try (//language=sql
+             Connection conn = ds.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(
+                     "SELECT * FROM activities WHERE id = ?"
+             )) {
+            stmt.setLong(1, id);
+
+            try (ResultSet rs = stmt.executeQuery()) {
+                if (rs.next()) {
+                    return new Activity(rs.getInt("id"),
+                            rs.getTimestamp("start_time").toLocalDateTime(),
+                            rs.getString("description"),
+                            ActivityType.valueOf(rs.getString("activity_type")));
+                }
+
+                throw new IllegalArgumentException("Activity with id " + id + " is not exist!");
+            }
+
 
         } catch (SQLException sqle) {
             throw new IllegalStateException("Unable to read DB", sqle);
